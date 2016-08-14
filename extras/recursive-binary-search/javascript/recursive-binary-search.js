5dbc29fba037530a86ff8fed5003780e8f6e1bac
Lansana Camara <lxc5296@gmail.com>
2016-08-14 19:47:26 -0400

    Recursive binary search

---
+++ extras/recursive-binary-search/javascript/recursive-binary-search.js
@@ -0,0 +1,28 @@
+/**
+ * Recursive binary search implementation
+ *
+ * @param list
+ * @param val
+ * @param low
+ * @param high
+ */
+function recursiveBinarySearch(list, val, low, high) {
+    if (low > high) {
+        return -1;
+    }
+
+    let mid = (low + high) >>> 1;
+
+    if (list[mid] === val) {
+        return mid;
+    } else if (mid > val) {
+        return recursiveBinarySearch(list, val, low, mid  -1);
+    } else if (mid < val) {
+        return recursiveBinarySearch(list, val, mid + 1, high);
+    }
+}
+
+let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+recursiveBinarySearch(list, 7, 0, list.length); // 6
+recursiveBinarySearch(list, 11, 0, list.length); // -1
