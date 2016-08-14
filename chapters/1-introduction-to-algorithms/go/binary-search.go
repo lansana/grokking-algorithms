b64e5a9d13b4bc6a757dc4d156256581afaa7a64
Lansana Camara <lxc5296@gmail.com>
2016-08-13 23:01:09 -0400

    Binary search

---
+++ chapters/1-introduction-to-algorithms/go/binary-search.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+var list = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+func binarySearch(list []int, val int) int {
+	low := 0
+	high := len(list) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+
+		if list[mid] == val {
+			return mid;
+		} else if list[mid] < val {
+			low = mid + 1;
+		} else if list[mid] > val {
+			high = mid - 1;
+		}
+
+		mid = (low + high) / 2
+	}
+
+	return -1
+}
+
+func main() {
+	fmt.Println(binarySearch(list, 1)) // 0
+	fmt.Println(binarySearch(list, 11)) // -1, not found
+}
