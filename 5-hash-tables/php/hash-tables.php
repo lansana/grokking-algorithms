34663458a2f24ffbf413ab7035bf18e5a8f306ee
Lansana Camara <lxc5296@gmail.com>
2016-08-21 10:05:44 -0400

    Hash table

---
+++ 5-hash-tables/php/hash-tables.php
@@ -0,0 +1,9 @@
+<?php
+
+$quotes = array(
+    'Bruce Lee' => 'Mistakes are always forgivable, if one has the courage to admit them.',
+    'Steve Jobs' => 'Innovation distinguishes between a leader and a follower.'
+);
+
+echo $quotes['Bruce Lee']; // Mistakes are always forgivable, if one has the courage to admit them.
+echo $quotes['Steve Jobs']; // Innovation distinguishes between a leader and a follower.
