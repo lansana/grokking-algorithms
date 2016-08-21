8d8a96ce7564f9f3aab243df0dbed1eb521e227b
Lansana Camara <lxc5296@gmail.com>
2016-08-21 09:47:24 -0400

    Hash table

---
+++ 5-hash-tables/javascript/hash-tables.js
@@ -0,0 +1,7 @@
+let HashTable = {};
+
+HashTable['Apple Cider'] = 2.99;
+HashTable['Orange Juice'] = 1.99;
+
+console.log(HashTable['Apple Cider']); // '2.99'
+console.log(HashTable['Orange Juice']); // '1.99'
